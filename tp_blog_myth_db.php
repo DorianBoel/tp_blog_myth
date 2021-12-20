b49948bad547f7cfe63b5e76e9bd0e9392c69dc9
DorianBoel <dorian.boel@free.fr>
2021-12-20 01:54:57 +0100

    fix phpmyadmin access

--- tp_blog_myth_db.php
+++ tp_blog_myth_db.php
@@ -1,6 +1,6 @@
 <?php 
 try {
-    $db = new PDO("mysql:host=localhost;dbname=tp_blog_myth;charset=utf8", "root", "root");
+    $db = new PDO("mysql:host=localhost;dbname=tp_blog_myth;charset=utf8", "root", "");
 } catch (Exception $e) {
     die($e->getMessage());
 }
